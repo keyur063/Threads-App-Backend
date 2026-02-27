772031c149e25c16b71a351b783928d48513d3e6
keyur063 <keyurmitbander@gmail.com>
2026-02-27 13:03:47 +0530

    Prisma Setup Complete

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import cors from 'cors';
+import { prismaClient } from './lib/db';
 
 async function init() {  
     const app = express();
@@ -14,11 +15,32 @@ async function init() {
                 hello: String
                 say(name: String!): String
             }
+            type Mutation {
+                createUser(
+                    firstName: String!,
+                    lastName: String!,
+                    email: String!,
+                    password: String!
+                ): Boolean
+            }
         `,
         resolvers: {
             Query: {
                 hello: () => 'Hey there',
-                say: (_, {name}: {name: String} ) => `Hey ${name}`,
+                say: (_, {name}: {name: string} ) => `Hey ${name}`,
+            },
+            Mutation: {
+                createUser: async(_, 
+                    { firstName, lastName, email, password }: 
+                    { firstName: string; lastName: string; email: string; password: string }) => {
+                        await prismaClient.user.create({
+                            data: {
+                                firstName, lastName, email, password, 
+                                salt: 'random_salt',
+                            },
+                        });
+                        return true;
+                    }
             },
         },
         plugins: [ ApolloServerPluginLandingPageLocalDefault() ],
